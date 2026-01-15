7e04fc8b2cf91161d5099be1725510b2b70d7f64
italo <italo.devv@gmail.com>
2026-01-15 20:26:26 -0300

    feat: Cria as rotas do MissoesController com o mock

--- src/main/java/com/konoha/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/konoha/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,34 @@
 package com.konoha.CadastroDeNinjas.Missoes;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 
 public class MissoesController {
+
+    // GET - mandar uma requisicao para mostrar as missoes
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso";
+    }
+
+    // POST - mandar uma requisicao para criar as missoes
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso";
+    }
+
+    // PUT - mandar uma requisicao para alterar as missoes
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso";
+    }
+
+    // DELETE - mandar uma requisicao para deletar as missoes
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
+
+
 }
