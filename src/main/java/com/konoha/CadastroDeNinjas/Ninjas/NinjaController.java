9cc66c142c53b2cefcc2d3646044c51114ed60b8
italo <italo.devv@gmail.com>
2026-01-15 20:13:04 -0300

    feat: Atualiza o controle com mock de dados

--- src/main/java/com/konoha/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/konoha/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -11,4 +11,34 @@ public class NinjaController {
         return "Essa é minha primeira mensagem nessa rota.";
     }
 
+    // Adicionar Ninja (Create)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Mostrar todos os ninjas (Read)
+    @GetMapping("/all")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar todos os ninjas";
+    }
+
+    // Mostrar Ninja por ID (Create)
+    @GetMapping("/allID")
+    public String mostrarTodosOsNinjasPorID(){
+        return "Mostrar todos os IDs ninjas";
+    }
+
+    // Alterar dados dos Ninjas (Update)
+    @PutMapping("/alterar")
+    public String alterarNinjaPorID(){
+        return "Alterar Ninja por ID";
+    }
+
+    // Deletar Ninja (Delete)
+    @DeleteMapping("/deleteID")
+    public String deletarNinjaPorID(){
+        return "Deletar Ninja por ID";
+    }
+
 }
